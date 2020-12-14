5346254bf2c4e634401e3ece4ee18f9043f135ba
racross1 <racross1@gmail.com>
2020-12-14 11:27:25 -0800

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :articles, only: [:index, :show, :new, :create]
+  resources :articles, only: [:index, :show, :new, :create, :edit, :update]
+  #Below 2 lines commented out b/c README notes that the below
+  #can be accomplished with adding :edit and :update actions to the resrouces call
+  #keeping the below as a reminder on how the format of those calls looks broken out 
+  # get 'articles/:id/edit', to: 'articles#edit', as: :edit_article
+  # patch 'articles/:id', to: 'articles#update'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Article.destroy_all
+a1 = Article.create(title: "My Article", description: "My article desc")
+a2 = Article.create(title: "Arty the Article", description: "It's Arty!")
+
+puts "done"
